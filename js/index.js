60187adb3299d151451d84534965906b2911339d
Santiago Chaves <santiagochaves242@gmail.com>
2023-03-26 22:35:46 -0300

    Finished first version

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 const canvas = document.querySelector('#canvas');
+const HEIGHT = 400;
+const WIDTH = 400;
+const DIST = 10;
+let x = 0;
+let y = 0;
 
 function draw() {
-  requestAnimationFrame(draw);
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    const ran = Math.random() < 0.5 ? 0 : 1;
+
+    if (ran) backSlash(ctx);
+    else forwardSlash(ctx);
+
+    x += DIST;
+    if (x > WIDTH) {
+      y += DIST;
+      x = 0;
+    }
+    requestAnimationFrame(draw);
+  }
 }
 
 draw();
+
+// Util functions
+function backSlash(ctx) {
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + DIST, y + DIST);
+  ctx.stroke();
+}
+
+function forwardSlash(ctx) {
+  ctx.moveTo(x, y + DIST);
+  ctx.lineTo(x + DIST, y);
+  ctx.stroke();
+}
